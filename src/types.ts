2ff2a95e05fb716c3183bb4d19dfca93f7d9897a
yappiverse <setiawannaufal0@gmail.com>
2025-09-20 12:23:07 +0700

    add cf turnstile

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,12 @@ export type Bindings = {
     DB: D1Database;
     URL: string;
     secretKey: string;
+    TURNSTILE_SECRET: string;
+};
+
+export type TurnstileVerifyResponse = {
+    success: boolean;
+    challenge_ts?: string;
+    hostname?: string;
+    "error-codes"?: string[];
 };
